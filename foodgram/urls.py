def6f3bc39e5569a18d72c962b4fe79aa2d95d0a
Jan Paralic <jparalic@gmail.com>
2018-02-22 11:26:25 +0000

    Setting up the index view

--- foodgram/urls.py
+++ foodgram/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.conf.urls import include
+
+from foodfeed import views
 
 urlpatterns = [
+    url(r"^$", views.index, name = "index"),
+    url(r"^foodfeed/", include("foodfeed.urls")),
     url(r'^admin/', admin.site.urls),
 ]
