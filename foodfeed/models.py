27c7a1e083e6731807c8c41ceafe41fa16bcd2ef
Jan Paralic <jparalic@gmail.com>
2018-02-27 15:35:16 +0000

    User registration implemented

--- foodfeed/models.py
+++ foodfeed/models.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import PermissionsMixin
 from django.contrib.auth.base_user import AbstractBaseUser
 from django.utils.translation import ugettext_lazy as _
 
-from .managers import UserManager
+from foodfeed.managers import UserManager
 
 class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(_('email address'), unique=True)
@@ -14,6 +14,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     last_name = models.CharField(_('last name'), max_length=30, blank=True)
     date_joined = models.DateTimeField(_('date joined'), auto_now_add=True)
     is_active = models.BooleanField(_('active'), default=True)
+    is_staff = models.BooleanField(_('staff'), default=False)
     avatar = models.ImageField(upload_to='avatars/', null=True, blank=True)
 
     objects = UserManager()
--- foodfeed/views.py
+++ foodfeed/views.py
@@ -1,4 +1,19 @@
-from django.shortcuts import render
+from django.contrib.auth import login, authenticate
+from django.shortcuts import render, redirect
+
+from foodfeed.forms import SignUpForm
 
 def index(request):
-    return render(request, "foodfeed/index.html")
+    registered = False
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            user.set_password(user.password)
+            user.save()
+            registered = True
+
+    else:
+        form = SignUpForm()
+
+    return render(request, "foodfeed/index.html", {"form": form, "registered": registered})
