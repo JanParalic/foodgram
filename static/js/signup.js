afef8a8495f302a0f31f44fa4975741bf59d7465
Matus <2265072g@student.gla.acuk>
2018-03-13 10:43:18 +0000

    more css stuff

--- static/js/signup.js
+++ static/js/signup.js
@@ -1,16 +1,17 @@
-
+window.onload = function(){
+    // your code
 
 // Get the modal
 var modal = document.getElementById('myModal');
 
 // Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+var btn = document.getElementById("sign");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks the button, open the modal
-btn.onclick = function() {
+document.getElementById("sign").onclick = function() {
     modal.style.display = "block";
 }
 
@@ -26,3 +27,5 @@ window.onclick = function(event) {
     }
 }
 
+
+};
