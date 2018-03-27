b10c3b824876c9da2d89ae78953010b87e90d22f
Jan Paralic <jparalic@gmail.com>
2018-03-27 21:21:19 +0200

    Comment ajax now working

--- static/js/rating_ajax.js
+++ static/js/rating_ajax.js
@@ -5,11 +5,10 @@ $(document).ready(function(){
          var value = $(this).attr("data-value");
          var index = $(this).attr("data-index");
          $.get("/foodfeed/make_rating/", {picture_slug: picSlug, type: type, value: value}, function(data){
-             $("#big_post" + index).hide();
          });
      });
 
-     $("#commentForm").submit(function(event){
+     $(".commentForm").submit(function(event){
          event.preventDefault();
          var values = $(this).serializeArray();
          var formValuesDict = {};
@@ -20,8 +19,11 @@ $(document).ready(function(){
 
          var picSlug = formValuesDict["picture"];
          var comment = formValuesDict["comment"];
+
+         var index = $(this).attr("data-index");
          $.get("/foodfeed/add_comment/", {picture_slug: picSlug, comment: comment}, function (data) {
-             $("#commentForm").html(data);
+             $("#comments." + index).append("<p class='comment'>" + comment + "</p>");
          });
+         $(this)[0].reset();
      });
 });
